e84c6ee2c287951ebde596f3b34676cfdd55c1eb
Abhi Rathod <abhirathod95@gmail.com>
2016-11-28 13:58:35 -0600

    Trying again

--- app/src/main/java/cs371m/laser_chess/SocketManager.java
+++ app/src/main/java/cs371m/laser_chess/SocketManager.java
@@ -6,7 +6,6 @@ import android.bluetooth.BluetoothSocket;
 
 /**
  * Created by daniel on 11/27/16.
- * They told me to use a service. I said nei, munum vio nota pennan static class.
  */
 
 public final class SocketManager extends Application {
